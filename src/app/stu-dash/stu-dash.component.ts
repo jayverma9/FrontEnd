36e6c94f35f77124a816e97830a9297b689155e7
jayverma9 <jay.verma@stonybrook.edu>
2019-10-29 19:24:46 -0400

    Setting up frontend 2

---
+++ src/app/stu-dash/stu-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stu-dash',
+  templateUrl: './stu-dash.component.html',
+  styleUrls: ['./stu-dash.component.css']
+})
+export class StuDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
