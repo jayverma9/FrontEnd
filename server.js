0e6cdb35eadcb5bd0d70dbb8a8e22dc9d64daa9e
jayverma9 <jay_verma99@yahoo.com>
2019-10-29 22:28:47 -0400

    me thak gaya hu ab(deploy)

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('*', (req, res) => {
-  res.sendFile('./FrontEnd/dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.set( 'port', ( process.env.PORT || 5000 ));
-
-// Start node server
-app.listen( app.get( 'port' ), function() {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
-  });
+app.listen(process.env.PORT || 8080);
