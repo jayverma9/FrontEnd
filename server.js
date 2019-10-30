aa024b13f278f29b6257d21cb78a67e575f125cb
jayverma9 <jay_verma99@yahoo.com>
2019-10-29 20:17:15 -0400

    heroku trying port changes

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ app.get('*', (req, res) => {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
