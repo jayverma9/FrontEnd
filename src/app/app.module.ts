9af0f00ca68c46e811e3dc560786d26ec4a76d20
Rohit Luthra <rohitluthra9@outlook.com>
2019-10-30 19:44:47 -0400

    Routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { LoginComponent } from './login/login.component';
 import { StuDashComponent } from './stu-dash/stu-dash.component';
 import { InstructorComponent } from './instructor/instructor.component';
 import { SignupComponent } from './signup/signup.component';
+import {Routes, RouterModule} from '@angular/router';
+
+// @ts-ignore
+const approutes: Routes = [
+
+  {path: '', component: LoginComponent},
+  {path: 'stuDash', component: StuDashComponent},
+  {path: 'instructor', component: InstructorComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +26,9 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Here, our routes are registered in angular app. and RouterModule gives us this funtionality.
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
