0957bd64dde5737b8054b12733307bb98f481273
Rohit Luthra <rohitluthra9@outlook.com>
2019-11-02 17:52:29 -0400

    Revert "Still Incomplete, DO NOT pull."
    This reverts commit b335a6e6666845aca0a8b03be8c2cc2ee0fa84fd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,6 @@ import { SignupComponent } from './signup/signup.component';
 import {Routes, RouterModule} from '@angular/router';
 import { GroceryComponent } from './grocery/grocery.component';
 import { StudentDashbardComponent } from './student-dashbard/student-dashbard.component';
-import { InstructorDashboardComponent } from './instructor-dashboard/instructor-dashboard.component';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {MatCardModule} from '@angular/material';
-
 
 // @ts-ignore
 const approutes: Routes = [
@@ -33,15 +29,12 @@ const approutes: Routes = [
     InstructorComponent,
     SignupComponent,
     GroceryComponent,
-    StudentDashbardComponent,
-    InstructorDashboardComponent
+    StudentDashbardComponent
   ],
   imports: [
     BrowserModule,
     // Here, our routes are registered in angular app. and RouterModule gives us this funtionality.
-    RouterModule.forRoot(approutes),
-    DragDropModule,
-    MatCardModule
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/student-dashbard/student-dashbard.component.ts
+++ src/app/student-dashbard/student-dashbard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-student-dashbard',
@@ -7,37 +6,13 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./student-dashbard.component.css']
 })
 export class StudentDashbardComponent implements OnInit {
-  constructor() { }
   isOpen: boolean;
-
-  fruits = [
-    'Strawberry',
-    'Apple',
-    'Banana',
-    'Apricots',
-    'Mango',
-    'Orange',
-  ];
-
-  workspace = [
-    'Tomtoes'
-  ];
+  constructor() { }
 
   ngOnInit() {
   }
 
   dropdownShowOrNot() {
     this.isOpen = !this.isOpen;
-  }
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    }
   }
 }
